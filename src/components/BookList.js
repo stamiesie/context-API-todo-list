d2223f24ed3bce9d159d61be48a0f93c11b5bb07
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-21 11:16:37 -0700

    add light/dark mode

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 export default class BookList extends Component {
+    static contextType = ThemeContext;
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className='book-list'>
+            <div className='book-list' style={{ background: theme.bg, color: theme.color }}>
                 <ul>
-                    <li>Desert Solitaire</li>
-                    <li>One Hundred Years of Solitude</li>
-                    <li>East of Eden</li>
+                    <li style={{ background: theme.ui} }>Desert Solitaire</li>
+                    <li style={{ background: theme.ui} }>One Hundred Years of Solitude</li>
+                    <li style={{ background: theme.ui} }>East of Eden</li>
                 </ul>
             </div>
         )
