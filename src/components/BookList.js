e57400dbb68b61780f68a3e1131ad523c3b5eb0b
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-10-21 13:27:14 -0700

    refactor BookList into functional component using useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,19 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-export default class BookList extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className='book-list' style={{ background: theme.bg, color: theme.color }}>
-                <ul>
-                    <li style={{ background: theme.ui} }>Desert Solitaire</li>
-                    <li style={{ background: theme.ui} }>One Hundred Years of Solitude</li>
-                    <li style={{ background: theme.ui} }>East of Eden</li>
-                </ul>
-            </div>
-        )
-    }
+// CLASS COMPONENT
+// export default class BookList extends Component {
+//     static contextType = ThemeContext;
+//     render() {
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return (
+//             <div className='book-list' style={{ background: theme.bg, color: theme.color }}>
+//                 <ul>
+//                     <li style={{ background: theme.ui} }>Desert Solitaire</li>
+//                     <li style={{ background: theme.ui} }>One Hundred Years of Solitude</li>
+//                     <li style={{ background: theme.ui} }>East of Eden</li>
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+
+// FUNCTION COMPONENT
+export default function BookList() {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className='book-list' style={{ background: theme.bg, color: theme.color }}>
+            <ul>
+                <li style={{ background: theme.ui} }>Desert Solitaire</li>
+                <li style={{ background: theme.ui} }>One Hundred Years of Solitude</li>
+                <li style={{ background: theme.ui} }>East of Eden</li>
+            </ul>
+        </div>
+    )
 }
+
